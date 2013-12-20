7858316fb565c42fbce6af07f50e0c65462c8ca4
Walter Heck <walterheck@olindata.com>
2013-12-20 23:43:25 +0100

    Making more configuration params available to galera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,17 @@
 
 class galera (
-  $cluster_name     = 'galera',
-  $master_ip        = false,
-  $package_name     = 'galera',
-  $wsrep_provider   = '/usr/lib/galera/libgalera_smm.so',
-  $wsrep_node_name  = $::hostname,
-  $wsrep_notify_cmd = '',
-  $wsrep_sst_method = 'xtrabackup',
-  $wsrep_sst_auth   = '',) {
+  $cluster_name                = 'galera',
+  $master_ip                   = false,
+  $package_name                = 'galera',
+  $wsrep_provider              = '/usr/lib/galera/libgalera_smm.so',
+  $wsrep_node_name             = $::hostname,
+  $wsrep_node_address          = $::ipaddress,
+  $wsrep_node_incoming_address = $::ipaddress,
+  $wsrep_notify_cmd            = '',
+  $wsrep_sst_method            = 'xtrabackup',
+  $wsrep_sst_auth              = 'root:root',
+  $wsrep_sst_receive_address   = $::ipaddress,
+  $wsrep_sst_donor             = $::hostname,) {
   package { 'galera':
     ensure  => present,
     name    => $package_name,
